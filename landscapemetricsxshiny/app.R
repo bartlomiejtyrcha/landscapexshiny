388e7b491a1b7950f016fc2f40532fdbdf1e1a57
Bartłomiej Tyrcha <bartlomiejtyrcha@gmail.com>
2021-05-23 17:13:34 +0200

    download xlsx, csv; reactive calculate_lsm

--- landscapemetricsxshiny/app.R
+++ landscapemetricsxshiny/app.R
@@ -1,9 +1,11 @@
 library(landscapemetrics)
 library(landscapetools)
+library(stringr)
 library(shiny)
 library(shinyWidgets)
 library(shinythemes)
 library(raster)
+library(openxlsx)
 library(dplyr)
 list_lsm = list_lsm()
 
@@ -49,10 +51,8 @@ ui <-
                         textInput("directions", "Directions", value = 8),
                         textOutput("results"),
                         selectInput("count_boundary", "count_boundary:",
-                                    c("TRUE" = TRUE,
-                                      "FALSE" = FALSE), selected = "FALSE"),
-                        #selectInput("consider_boundary", "consider_boundary:", 
-                          #          c("TRUE" = TRUE, "FALSE" = FALSE), selected = "FALSE")
+                                    c(TRUE, FALSE), selected = FALSE),
+                        selectInput("consider_boundary", "consider_boundary:", c("TRUE" = TRUE, "FALSE" = FALSE), selected = "FALSE"),
                         textInput("edge_depth", "edge_depth", value = 1),
                         selectInput("cell_center", "cell_center", c("TRUE" = TRUE, "FALSE" = FALSE), selected = "FALSE"),
                         textInput("classes_max", "classes_max", value = NULL),
@@ -64,29 +64,28 @@ ui <-
                         selectInput("progress", "progress", c("TRUE" = TRUE, "FALSE" = FALSE), selected = "FALSE")),
                         actionButton("run", label = "Run")
                         ), 
+     tabPanel("Moving window"),
     tabPanel("Results",
              mainPanel(
-               downloadButton("downloadData", "Download CSV")
+               downloadButton("downloadDataCSV", "Download CSV"),
+               downloadButton("downloadDataXLSX", "Download XSLX")
              ),
              fluidRow(
                column(12, 
                       dataTableOutput('calculate') # calculate_lsm() output
                       )
                )
-             )
+             ),
+    tabPanel("next tab panel")
     ))
     
 server = function(input, output){
     inFile = reactive({
         raster::raster(input$file1$datapath) # Upload table, input - reactive()
     })
-    observeEvent(input$run, {
-    #output$mapPlot = renderPlot( #Plot raster object
-    #    {
-    #        plot(inFile());
-    #    }
-    #)
     output$rasterPlot = renderPlot({plot(inFile())})
+    output$checklandscapeTable = renderDataTable(check_landscape(inFile())) # renderTable check_landscape()
+    observeEvent(input$run, {
     output$plot = renderPlot({{
         func_plot = function(input){
             if(input == "Landscape"){
@@ -102,57 +101,56 @@ server = function(input, output){
                 plot(inFile())
             }
             else if(input == "Correlation"){
-                show_correlation(calculate_lsm(inFile(), level = input$level, 
-                                               name = input$name, type = input$type, 
-                                               what = input$function_name, directions = input$directions,
-                                               count_boundary = input$count_boundary,
-                                               #consider_boundary = input$consider_boundary
-                                               edge_depth = input$edge_depth,
-                                               cell_center = input$cell_center,
-                                               classes_max = input$classes_max,
-                                               neighbourhood = input$neighbourhood,
-                                               ordered = input$ordered,
-                                               base = input$base,
-                                               full_name = input$full_name,
-                                               verbose = input$verbose,
-                                               progress = input$progress
-                ))
+                show_correlation(Calculate()
+                )
           }
         }
     }
     func_plot(input$optionplot)}
         
     )
-    #output$PlotLandscape = renderPlot({show_landscape(inFile())})
-    #output$PlotPatch = renderPlot({show_patches(inFile())})
-    #output$PlotCore = renderPlot({show_cores(inFile())})
-    output$checklandscapeTable = renderDataTable(check_landscape(inFile())) # renderTable check_landscape()
-    output$calculate = renderDataTable(calculate_lsm(inFile(), level = input$level, 
-                                                     name = input$name, type = input$type, 
-                                                     what = input$function_name, directions = input$directions,
-                                                     count_boundary = input$count_boundary,
-                                                     #consider_boundary = input$consider_boundary
-                                                     edge_depth = input$edge_depth,
-                                                     cell_center = input$cell_center,
-                                                     classes_max = input$classes_max,
-                                                     neighbourhood = input$neighbourhood,
-                                                     ordered = input$ordered,
-                                                     base = input$base,
-                                                     full_name = input$full_name,
-                                                     verbose = input$verbose,
-                                                     progress = input$progress
-                                                     )) # calculatelsm()
-    output$downloadData <- downloadHandler(
+    Calculate = reactive({
+      landscapemetrics::calculate_lsm(inFile(), level = input$level, 
+                                      name = input$name, type = input$type, 
+                                      what = input$function_name, directions = input$directions,
+                                      count_boundary = input$count_boundary,
+                                      consider_boundary = input$consider_boundary,
+                                      edge_depth = input$edge_depth,
+                                      cell_center = input$cell_center,
+                                      classes_max = input$classes_max,
+                                      neighbourhood = input$neighbourhood,
+                                      ordered = input$ordered,
+                                      base = input$base,
+                                      full_name = input$full_name,
+                                      verbose = input$verbose,
+                                      progress = input$progress
+      ) # Upload table, input - reactive()
+    })
+
+    output$calculate = renderDataTable(Calculate()) # calculatelsm()
+    output$downloadDataCSV <- downloadHandler(
     filename = function() {
-      paste(input$calculate, ".csv", sep = "")
+      paste0(Sys.time() %>% str_replace_all(
+        pattern = "\\-",replacement = "\\_") %>% str_replace_all(
+          pattern = "\\:", replacement = "\\") %>% str_replace(
+            pattern = "\\ ", replacement = "\\_"), "_calculated.csv")
     },
     content = function(file) {
-      write.csv(calculate_lsm(inFile(), level = input$level, 
-                              name = input$name, type = input$type, 
-                              what = input$function_name, directions = input$directions,
-                              count_boundary = input$count_boundary), file, row.names = FALSE)
+      write.csv(Calculate(), file, row.names = FALSE)
     }
   )
+    output$downloadDataXLSX <- downloadHandler(
+      filename = function(){
+        paste0(Sys.time() %>% str_replace_all(
+          pattern = "\\-",replacement = "\\_") %>% str_replace_all(
+            pattern = "\\:", replacement = "\\") %>% str_replace(
+              pattern = "\\ ", replacement = "\\_"), "_calculated",".xlsx")
+      },
+      content = function(file) {
+        openxlsx::write.xlsx(Calculate(), file)
+      }
+    )
+
     })}
 
             
--- test.R
+++ test.R
@@ -1,20 +1,32 @@
 pakiety = c('landscapemetrics', 'shiny', 'landscapetools', 'raster')
 install.packages(pakiety)
+install.packages("raster", type = "source")
+install.packages("igraph")
+landscapes = raster("example_raster/landscapes.tif")
+#install.packages("NLMR")
+devtools::install_github("ropensci/NLMR")
+install.packages("libproj")
+install.packages("gdal")
+install.packages("xlsx")
+library(rgdal)
 library(shiny)
 library(landscapemetrics)
 library(landscapetools)
 library(raster)
 library(dplyr)
 library(stringr)
-landscapes = raster("example_raster/landscapes.tif")
-
-
+library(igraph)
+library(NLMR)
+library(xlsx)
+library(libproj)
 list_lsm = list_lsm()
 patch = filter(list_lsm, level == "patch")
 landscape = filter(list_lsm, level == "landscape")
 class = filter(list_lsm, level == "class")
 
 #list_lsm$function_name
+
+
 
 View(list_lsm)
 show_landscape(augusta)
@@ -56,3 +68,31 @@ landscapes = raster("example_raster/raster_1.tif")
 calculate_lsm(example_raster, what = "lsm_c_area_sd", count_boundary = A,  edge_depth = 14)
 calculate_lsm(landscapes, what = "lsm_c_area_sd", count_boundary = A, consider_boundary = FALSE, edge_depth = 14)
 
+set.seed(2021-05-23)
+a = nlm_fbm(50, 50, resolution = 100, fract_dim = 0.5)
+plot(a)
+str(a)
+writeRaster(a, "example_20210523", format = "GTiff", overwrite = TRUE)
+
+plot(raster("example_20210523.tif"))
+
+
+set.seed(2021-05-22)
+a = nlm_fbm(50, 50, resolution = 100, fract_dim = 0.5)
+plot(a)
+
+Sys.time() %>% str_replace_all(
+  pattern = "\\-",replacement = "\\_") %>% str_replace_all(
+    pattern = "\\:", replacement = "\\") %>% str_replace(
+      pattern = "\\ ", replacement = "\\_")
+
+paste0(Sys.time() %>% str_replace_all(
+  pattern = "\\-",replacement = "\\_") %>% str_replace_all(
+    pattern = "\\:", replacement = "\\") %>% str_replace(
+      pattern = "\\ ", replacement = "\\_"), "_calculated.csv")
+calculate_lsm(a, what = "lsm_c_area_sd")
+
+write.xlsx(calculate_lsm(a, what = "lsm_c_area_sd"), file = paste0(Sys.time() %>% str_replace_all(
+  pattern = "\\-",replacement = "\\_") %>% str_replace_all(
+    pattern = "\\:", replacement = "\\") %>% str_replace(
+      pattern = "\\ ", replacement = "\\_"), "_calculated",".xlsx"), row.names = FALSE)
